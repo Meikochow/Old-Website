c6a2eb17efc45ee7ac476d940cda76c8a57bc198
Victor Cojocaru <victorcojocaru93@gmail.com>
2018-12-21 15:30:59 -0600

    first real update

---
+++ public/navBar.js
@@ -0,0 +1,40 @@
+function	toggle() {
+		let linksEl = document.querySelector('.narrowLinks');
+		if (linksEl.style.display === 'block') {
+			linksEl.style.display = 'none';
+		} else {
+			linksEl.style.display = 'block';
+		}
+	}
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top-40
+        }, 1200
+                                , function() {
+        //   // Callback after animation
+        //   // Must change focus!
+        var $target = $(target);
+        $target.focus();
+         }
+      );
+      }
+    }
+  });
---
+++ public/script.js
@@ -0,0 +1,158 @@
+let projects={
+    1:{
+   projectName :"Anonymous message board",
+   projectDescription:"Full-Stack app that allows anonymous users to create custom boards, threads and messages",
+   projectThumbNailLink :"https://i.postimg.cc/HLgWVzg9/anon-board.png",
+   projectTech :`<i>Built using Node, Express, MongoDB, JS+jQuery, HelmetJs, Bcrypt, Mocha, CSS3 and HTML5</i>`,
+   projectHref :"https://surf-code.glitch.me/",
+  },
+  2:{
+   projectName :"Issue Tracker",
+   projectDescription:"Full-Stack app that allows users to post, manage and reply to work related issues.",
+   projectThumbNailLink :"https://i.postimg.cc/2jVG5Rxm/issue-tracker.png",
+   projectTech :`<i>Built using Node, Express, MongoDB, JS+jQuery, HelmetJs, Mocha, CSS3 and HTML5</i>`,
+   projectHref :"https://rainbow-jump.glitch.me/apitest/",
+  },
+  3:{
+   projectName :"URL Shortener Microservice",
+   projectDescription:"Full-Stack app that accepts valid urls and returns a functional shortened version. ",
+   projectThumbNailLink :"https://i.postimg.cc/5NRt7dJM/Untitled.png",
+   projectTech :`<i>Built using Node, Express, MongoDB, JS+jQuery, Mocha, CSS3 and HTML5</i>`,
+   projectHref :"https://time-snipe.glitch.me/",   
+  },
+  4:{
+   projectName :"Personal library",
+   projectDescription:"Full-Stack app that allows the user to store book titles and write reviews.",
+   projectThumbNailLink :"https://i.postimg.cc/4dKcXZRS/personal-library.png",
+   projectTech :`<i>Built using Node, Express, MongoDB, JS+jQuery,HelmetJs, Mocha, CSS3 and HTML5</i>`,
+   projectHref :"https://meadow-swordfish.glitch.me/",
+  },
+  5:{
+   projectName :"Stock Price Checker",
+   projectDescription:"Full-Stack app that fetches real time stock price from a third-party api and saves the users' likes of the given stock in the DB. ",
+   projectThumbNailLink :"https://i.postimg.cc/jd67xZBt/stock-price-checker.png",
+   projectTech :`<i>Built using Node, Express, MongoDB, JS+jQuery,HelmetJs, Mocha, CSS3 and HTML5</i>`,
+   projectHref :"https://charming-cilantro.glitch.me/"
+  }, 
+  6:{
+   projectName :"Metric ↔ Imperial unit converter",
+   projectDescription:"Full-Stack app built using Node and Express which converts imperial units to metrics and vice versa.",
+   projectThumbNailLink :"https://i.postimg.cc/k5wr73vK/metric-imperial-converter.png",
+   projectTech :`<i>Built using Node, Express, MongoDB, JS+jQuery,HelmetJs, Mocha, CSS3 and HTML5</i>`,
+   projectHref :"https://apple-pear-1.glitch.me/",
+  },
+  7:{
+   projectName :"Pomodoro Clock",
+   projectDescription:"A time management, responsive React app based on the timeboxing concept, that provides customizable countdowns for working sessions.",
+   projectThumbNailLink :"https://s20.postimg.cc/547hsh9vh/React_Project_5.png",
+   projectTech :`<i>Built using ReactJs, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/BPZEGz/",
+  },
+  8:{
+   projectName :"Calculator",
+   projectDescription:"Responsive React app used to perform basic arithmetic calculations.",
+   projectThumbNailLink :"https://s20.postimg.cc/vpa0nz731/React_Project_4.png",
+   projectTech :`<i>Built using ReactJs, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/PBGKPQ/",
+  },
+  9:{
+   projectName :"Drum Machine",
+   projectDescription:"Responsive React electronic percussion instrument, allowing the user to play various sound elements via keys, mouse or touch.",
+   projectThumbNailLink :"https://s20.postimg.cc/5gyvym2f1/React_Project_3.png",
+   projectTech :`<i>Built using ReactJs, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/WyVqQp/",
+  },
+  10:{
+   projectName :"Markdown Previewer",
+   projectDescription:"Responsive Markdown markup language React App, featuring a side-by-side preview window rendering the code directly in a WYSIWYG fashion.",
+   projectThumbNailLink :"https://i.postimg.cc/RZsB5JQ4/markdown-previewer.png",
+   projectTech :`<i>Built using ReactJs, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/eKwpXO/",
+  },
+  11:{
+   projectName :"Random Quote generator",
+   projectDescription:"React app that uses a herokuapp.com Api to render a new quote upon click, which can be shared on twitter.",
+   projectThumbNailLink :"https://s20.postimg.cc/6w0gncb7x/React_Project_1.png",
+   projectTech :`<i>Built using ReactJs, Ajax, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/QxRXoY/"
+  },
+  12:{
+   projectName :"Local Weather App",
+   projectDescription:"Responsive jQuery app that uses the HTML geolocation of your device to present the weather conditions of your location.",
+   projectThumbNailLink :"https://i.postimg.cc/N031TbHN/weather-app.png",
+   projectTech :`<i>Built using JavaScript + jQuery, Ajax, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/oEmgXa/",
+  },
+    13:{
+   projectName :"Fold-Wiki",
+   projectDescription:"Responsive App that accepts user input and renders the 20 most relatable Wikipedia articles by using the Wiki Api.",
+   projectThumbNailLink :"https://i.postimg.cc/TwfhNLhM/wiki-Viewer.png",
+   projectTech :`<i>Built using JavaScript + jQuery, Ajax, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/EQBWgV/"
+  },
+    14:{
+   projectName :"Twitch TV status App",
+   projectDescription:"Responsive Twitch.tv Api rendering App that gets your custom streamers' data and presents it to the user.",
+   projectThumbNailLink :"https://i.postimg.cc/mkWXYq9L/twitch-Api.png",
+   projectTech :`<i>Buit using JavaScript + jQuery, Ajax, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/OvVzvM/"
+  },
+    15:{
+   projectName :"Tribute Page to Asani Inio",
+   projectDescription:"A responsive Tribute Web Page, based on applied accessibility guidelines",
+   projectThumbNailLink :"https://s20.postimg.cc/xh2zixg65/Tribute_Page.png",
+   projectTech :`<i>Built using JavaScript + jQuery, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/PaNpvB/"
+  },
+    16:{
+   projectName :"Tic Tac Toe",
+   projectDescription:"Tic-Tac-Toe game based on the MinMax Algorithm, designed with a customizable difficulty level which can be shared on your facebook.",
+   projectThumbNailLink :"https://s20.postimg.cc/kcxf68gel/Tic_Tac_Toe.png",
+   projectTech :`<i>Built using JavaScript + jQuery, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/odoqpe/"
+  },
+  17:{
+   projectName :"Simon Game",
+   projectDescription:"Simon Game app that creates a series of sounds and lights which require the user to repeat the sequence, based on their memory.",
+   projectThumbNailLink :"https://s20.postimg.cc/ebzq95ect/Simon_Game.png",
+   projectTech :`<i>Buit using JavaScript + jQuery, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/OZEMgz/"
+  },
+      18:{
+   projectName :"Morty Run",
+   projectDescription:"A platform Rick & Morty themed obstacle game.",
+   projectThumbNailLink :"https://s20.postimg.cc/bhwkvo1vx/Morty_Run.png",
+   projectTech :`<i>Built using JavaScript + jQuery, Canvas, SCSS and HTML5</i>`,
+   projectHref :"https://rawgit.com/Meikochow/Morty-Run/master/index.html"
+  },
+      19:{
+   projectName :"Snake",
+   projectDescription:"A Javascript Snake game",
+   projectThumbNailLink :"https://s20.postimg.cc/o9ar28gtp/Snake_JS.png",
+   projectTech :`<i>Built using JavaScript + jQuery, Canvas, SCSS and HTML5</i>`,
+   projectHref :"https://rawgit.com/Meikochow/Snake-Game-JavaScript/master/index.html"
+  },
+      20:{
+   projectName :"Math Marathon",
+   projectDescription:"Responsive ReactJs game of selecting the correct result against time, with a shareable score",
+   projectThumbNailLink :"https://s20.postimg.cc/ua8fz8qkd/Math_Marathon.png",
+   projectTech :`<i>Built using ReactJs, SCSS and HTML5</i>`,
+   projectHref :"https://codepen.io/Meikochow/full/YjBJmg/"
+  }
+      }
+    
+function showProject(event){
+ document.getElementById('portfolio').innerHTML='';
+ let eventData = event.split('-');
+ let beginList = parseInt(eventData[0]);
+ let endList = parseInt(eventData[1])
+  for(let i=beginList;i<=endList;i++){
+    document.getElementById('portfolio').innerHTML +=` <a href=${projects[i].projectHref} target="_blank"><div  style="background-image:url('${projects[i].projectThumbNailLink}');" alt=${projects[i].alt} class="projectThumbnail project"> <p class="projectsDescription"><span class="projectName">${projects[i].projectName}</span><span class="projectTech">${projects[i].projectTech}</span><span class="projDesc">${projects[i].projectDescription}</span><p> </div></a>`;
+ }
+ let tabLinkList = document.getElementsByClassName("tablink");
+  for(let j=0;j<tabLinkList.length;j++){
+    $(tabLinkList[j]).css({'background':'#e6f7ff','box-shadow':'0px 0px 20px','border':'1px solid white','border-radius':'10px'});
+  } 
+  $(`#${beginList}${endList}`).css({'background':'white','box-shadow':'0px 0px 40px','border':'3px solid','border-radius':'0px'});
+}
+showProject('1-6');
